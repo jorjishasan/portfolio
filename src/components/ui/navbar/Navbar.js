e673b2fd0e91d70501f04182a3c28c94ba0a799e
jorjishasan <dev.jorjis@gmail.com>
2024-12-07 05:23:22 +0600

    feat: add Navbar component for improved navigation layout

---
+++ src/components/ui/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import { maxContainerWidth } from "@/utils/constants";
+import LeftNav from "./LeftNav";
+import QuickLinksMenu from "./QuickLinksMenu";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="relative mx-auto flex items-center justify-between px-2 py-4 transition-all duration-300 lg:px-0"
+      style={{ maxWidth: `${maxContainerWidth}px` }}
+    >
+      <LeftNav />
+      <QuickLinksMenu />
+    </nav>
+  );
+};
+
+export default Navbar;
