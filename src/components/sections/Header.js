b6cd9423246c7d1213541d35d239543d8690fb78
jorjishasan <dev.jorjis@gmail.com>
2024-12-07 05:24:13 +0600

    feat: enhance Header component with scroll progress and improved layout

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,10 +1,22 @@
-import Navbar from "@ui/Navbar";
-
+"use client";
+import React from "react";
+import cn from "@/utils/cn";
+import Navbar from "@ui/navbar/Navbar";
+import useScrollProgress from "../../hooks/useScrollProgress";
+import ProgressBar from "../ui/navbar/ProgressBar";
 const Header = () => {
+  const { isScrolled, scrollYProgress } = useScrollProgress();
+
   return (
-    <div>
+    <header
+      className={cn(
+        "fixed top-0 z-50 w-full bg-white/90 backdrop-blur-xl dark:bg-gray-900/90",
+        isScrolled && "shadow-md",
+      )}
+    >
       <Navbar />
-    </div>
+      <ProgressBar progress={scrollYProgress} />
+    </header>
   );
 };
 
