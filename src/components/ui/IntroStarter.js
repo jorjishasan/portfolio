085d4e581ce82f2ba7e3fa5816c52449c8310356
jorjishasan <dev.jorjis@gmail.com>
2024-12-08 03:26:35 +0600

    feat: add IntroStarter component for displaying introductory content with tag, title, and description

---
+++ src/components/ui/IntroStarter.js
@@ -0,0 +1,15 @@
+const IntroStarter = ({ tag, title, description }) => {
+  return (
+    <div className="mx-auto mb-12 w-full text-center">
+      <span className="mb-2 inline-block bg-purple-500/10 px-4 py-2 text-[10px] font-bold uppercase text-purple-800">
+        {tag}
+      </span>
+      <h2 className="text-2xl font-bold md:text-4xl lg:text-4xl">{title}</h2>
+      <p className="mx-auto mt-4 max-w-xs text-center text-sm text-gray-500/80 md:max-w-sm md:text-left md:text-sm lg:max-w-md lg:text-base">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default IntroStarter;
