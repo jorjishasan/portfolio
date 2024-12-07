20d043b2165d80f638ca5c4cfd2a8e1c18f7d16e
jorjishasan <dev.jorjis@gmail.com>
2024-12-08 03:26:57 +0600

    feat: add project configuration data for multiple projects with descriptions and links

---
+++ src/components/ui/projects/projectConfig.js
@@ -0,0 +1,46 @@
+export const projectData = [
+  // {
+  //   name: "Fastkeys.dev",
+  //   description: "Description of project 1",
+  //   links: {
+  //     github: "https://github.com/yourusername/project1",
+  //     live: "https://yourproject1.com",
+  //     youtube: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  //   },
+  //   embed: `<iframe width="1280" height="720" src="https://www.youtube.com/embed/vFW_TxKLyrE" title="Expo in 100 Seconds" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+  //   tags: ["React", "Next.js", "Tailwind CSS"],
+  // },
+  {
+    name: "Algoview.app",
+    description: "Description of project 1",
+    links: {
+      github: "https://github.com/yourusername/project1",
+      live: "https://yourproject1.com",
+      youtube: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    },
+    embed: `<iframe width="1280" height="720" src="https://www.youtube.com/embed/vFW_TxKLyrE" title="Expo in 100 Seconds" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    tags: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    name: "Cornflix.app",
+    description: "Description of project 1",
+    links: {
+      github: "https://github.com/yourusername/project1",
+      live: "https://yourproject1.com",
+      youtube: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    },
+    embed: `<iframe width="1280" height="720" src="https://www.youtube.com/embed/vFW_TxKLyrE" title="Expo in 100 Seconds" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    tags: ["React", "Next.js", "Tailwind CSS"],
+  },
+  {
+    name: "zestybite.app",
+    description: "Description of project 1",
+    links: {
+      github: "https://github.com/yourusername/project1",
+      live: "https://yourproject1.com",
+      youtube: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    },
+    embed: `<iframe width="1280" height="720" src="https://www.youtube.com/embed/vFW_TxKLyrE" title="Expo in 100 Seconds" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+    tags: ["React", "Next.js", "Tailwind CSS"],
+  },
+];
