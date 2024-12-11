51bc20c3b4ff1659da38be608e29534b5eb3b970
jorjishasan <dev.jorjis@gmail.com>
2024-12-12 02:31:25 +0600

    feat: configure remote image patterns for optimized loading from external sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
