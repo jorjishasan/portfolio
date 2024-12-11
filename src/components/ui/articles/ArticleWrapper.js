f9fe0daa12486df131c248dfe97c4899eddf8b0f
jorjishasan <dev.jorjis@gmail.com>
2024-12-12 02:35:44 +0600

    feat: add ArticleWrapper component for responsive article display

---
+++ src/components/ui/articles/ArticleWrapper.js
@@ -0,0 +1,31 @@
+"use client";
+import useGetArticles from "@/hooks/useGetArticles";
+import MobileArticles from "./MobileArticles";
+import DesktopArticles from "./DesktopArticles";
+import IntroStarter from "../IntroStarter";
+import { sectionIntros } from "@/utils/constants";
+const ArticleWrapper = () => {
+  const { articles, loading } = useGetArticles();
+
+  if (loading) {
+    return <div>Loading articles...</div>;
+  }
+
+  const filteredArticles = articles
+    .filter((article) => article.cover_image)
+    .sort((a, b) => b.public_reactions_count - a.public_reactions_count);
+
+  return (
+    <div className="w-full">
+      <IntroStarter {...sectionIntros.articles} />
+      <div className="hidden md:block">
+        <DesktopArticles articles={filteredArticles} />
+      </div>
+      <div className="block md:hidden">
+        <MobileArticles articles={filteredArticles} />
+      </div>
+    </div>
+  );
+};
+
+export default ArticleWrapper;
