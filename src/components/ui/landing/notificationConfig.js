ad80abc721e6999e2d73b1da99e90d93987ad83f
jorjishasan <dev.jorjis@gmail.com>
2024-12-12 02:37:08 +0600

    feat: add play and pause SVG icons and notification configuration for project management

---
+++ src/components/ui/landing/notificationConfig.js
@@ -0,0 +1,50 @@
+export const notifications = [
+  {
+    name: "Ideation & Planning",
+    description: "Sync plan with project.",
+    icon: "🗞️",
+    color: "#8AC6FF", // Light Blue
+  },
+  {
+    name: "UI/UX Design",
+    description: "40% upheld",
+    icon: "🧠",
+    color: "#FFD78A", // Light Peach
+  },
+  {
+    name: "Full-Stack Dev",
+    description: "MERN, Perfect Stack Match.",
+    icon: "💸",
+    color: "#93E5D7", // Soft Mint
+  },
+  {
+    name: "Cloud Solutions",
+    description: "AWS, GCP, Vercel.",
+    icon: "☁️",
+    color: "#FF9EAE", // Soft Pink
+  },
+  {
+    name: "Testing & QA",
+    description: "Jest to Test.",
+    icon: "🧪",
+    color: "#B4E4FF", // Soft Sky Blue
+  },
+  {
+    name: "MVP Implemented",
+    description: "Idea to Product.",
+    icon: "💼",
+    color: "#FFD1DA", // Pastel Pink
+  },
+  {
+    name: "SEO Victory",
+    description: "SEO, SEO, SEO.",
+    icon: "✌🏾",
+    color: "#E7FFAC", // Light Lime
+  },
+  {
+    name: "Analytics Insight",
+    description: "Analytics built-in.",
+    icon: "📈",
+    color: "#FFF6BD", // Soft Yellow
+  },
+];
