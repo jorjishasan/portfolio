b6adac1978e9da78a8952d06a4e947ca6a8879fa
jorjishasan <dev.jorjis@gmail.com>
2024-12-12 02:34:06 +0600

    feat: add Articles component to render article wrappers

---
+++ src/components/sections/Articles.js
@@ -0,0 +1,13 @@
+"use client";
+
+import ArticleWrapper from "../ui/articles/ArticleWrapper";
+
+const Articles = () => {
+  return (
+    <div className="mx-auto mt-16 px-4 py-8 md:mt-24 md:px-8 lg:mt-36 xl:mt-40">
+      <ArticleWrapper />
+    </div>
+  );
+};
+
+export default Articles;
