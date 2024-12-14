e1266b87ad5329ad4ab6be6262d09a43597c8ab2
jorjishasan <dev.jorjis@gmail.com>
2024-12-15 00:00:33 +0600

    feat(style): update IntroStarter component to change tag text color from purple-800 to purple-500

--- src/components/ui/IntroStarter.js
+++ src/components/ui/IntroStarter.js
@@ -1,7 +1,7 @@
 const IntroStarter = ({ tag, title, description }) => {
   return (
     <div className="mx-auto mb-16 w-full text-center">
-      <span className="mb-2 inline-block bg-purple-500/10 px-4 py-2 text-[10px] font-bold uppercase text-purple-800">
+      <span className="mb-2 inline-block bg-purple-500/10 px-4 py-2 text-[10px] font-bold uppercase text-purple-500">
         {tag}
       </span>
       <h2 className="text-2xl font-bold md:text-4xl lg:text-4xl">{title}</h2>
