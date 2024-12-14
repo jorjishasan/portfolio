c9fc5a23311615cf5be895b3f90e591b747d63f7
jorjishasan <dev.jorjis@gmail.com>
2024-12-15 03:16:37 +0600

    feat: add skillsConfig module to define frontend, backend, and database skills

---
+++ src/components/ui/skills/skillsConfig.js
@@ -0,0 +1,30 @@
+const skillsConfig = {
+  frontend: [
+    {
+      name: "React",
+      desc: "frontend library",
+    },
+    { name: "Redux", desc: "state management" },
+    { name: "Next.js", desc: "frontend framework" },
+    { name: "Figma", desc: "wireframe, design, prototype" },
+    {
+      name: "Tailwind",
+      desc: "css framework",
+    },
+    { name: "Typescript", desc: "javascript superset" },
+    { name: "ShadCn", desc: "ui library" },
+  ],
+  database: [
+    { name: "MySQL", desc: "relational database" },
+    { name: "MongoDB", desc: "nosql database" },
+    { name: "SupaBase", desc: "postgres database" },
+    { name: "Firebase", desc: "realtime database" },
+  ],
+  backend: [
+    { name: "Node.js", desc: "javascript runtime" },
+    { name: "Express", desc: "backend framework" },
+    { name: "Jira", desc: "project management" },
+  ],
+};
+
+export default skillsConfig;
