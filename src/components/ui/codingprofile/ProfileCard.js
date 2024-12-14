9cce5553688395bef7b6eff1397ec9f0ca3daa83
jorjishasan <dev.jorjis@gmail.com>
2024-12-15 03:14:07 +0600

    feat: add ProfileCard component with interactive gradient effect

---
+++ src/components/ui/codingprofile/ProfileCard.js
@@ -0,0 +1,97 @@
+"use client";
+
+import React, { useCallback, useEffect, useRef } from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import cn from "@/utils/cn";
+import Image from "next/image";
+import Link from "next/link";
+
+const ProfileCard = ({
+  profile,
+  className,
+  gradientSize = 200,
+  gradientColor = "#262626",
+  gradientOpacity = 0.8,
+}) => {
+  const { name, icon, href } = profile;
+
+  const cardRef = useRef(null);
+  const mouseX = useMotionValue(-gradientSize);
+  const mouseY = useMotionValue(-gradientSize);
+
+  const handleMouseMove = useCallback(
+    (e) => {
+      if (cardRef.current) {
+        const { left, top } = cardRef.current.getBoundingClientRect();
+        mouseX.set(e.clientX - left);
+        mouseY.set(e.clientY - top);
+      }
+    },
+    [mouseX, mouseY],
+  );
+
+  const handleMouseOut = useCallback(
+    (e) => {
+      if (!e.relatedTarget) {
+        document.removeEventListener("mousemove", handleMouseMove);
+        mouseX.set(-gradientSize);
+        mouseY.set(-gradientSize);
+      }
+    },
+    [handleMouseMove, mouseX, gradientSize, mouseY],
+  );
+
+  const handleMouseEnter = useCallback(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    mouseX.set(-gradientSize);
+    mouseY.set(-gradientSize);
+  }, [handleMouseMove, mouseX, gradientSize, mouseY]);
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseout", handleMouseOut);
+    document.addEventListener("mouseenter", handleMouseEnter);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseout", handleMouseOut);
+      document.removeEventListener("mouseenter", handleMouseEnter);
+    };
+  }, [handleMouseEnter, handleMouseMove, handleMouseOut]);
+
+  useEffect(() => {
+    mouseX.set(-gradientSize);
+    mouseY.set(-gradientSize);
+  }, [gradientSize, mouseX, mouseY]);
+
+  return (
+    <div
+      ref={cardRef}
+      className={cn(
+        "group relative flex size-full cursor-pointer items-center justify-center overflow-hidden rounded-xl border border-gray-900 bg-neutral-900/30 bg-clip-padding px-8 py-6 backdrop-blur-md backdrop-saturate-150 backdrop-filter md:py-10 lg:py-12 xl:py-16",
+        className,
+      )}
+    >
+      <Link href={href} target="_blank">
+        <Image
+          src={icon}
+          alt={name}
+          width={100}
+          height={100}
+          className="relative z-10 h-auto w-10 md:w-16"
+        />
+      </Link>
+      <motion.div
+        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(${gradientSize}px circle at ${mouseX}px ${mouseY}px, ${gradientColor}, transparent 100%)
+          `,
+          opacity: gradientOpacity,
+        }}
+      />
+    </div>
+  );
+};
+
+export default ProfileCard;
