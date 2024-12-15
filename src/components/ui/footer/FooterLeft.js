972e6149a6841d5aa6529ac09a971985cc65a4b1
jorjishasan <dev.jorjis@gmail.com>
2024-12-15 17:45:10 +0600

    feat: add FooterLeft component with input box for user interaction

---
+++ src/components/ui/footer/FooterLeft.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import footerDecoration from "../../../../public/footer_decoration.svg";
+const FooterLeft = () => {
+  return (
+    <div className="w-full md:w-auto">
+      <Image
+        src={footerDecoration}
+        width={70}
+        height={50}
+        alt="footer decoration"
+      />
+      <InputBox />
+    </div>
+  );
+};
+
+const InputBox = () => {
+  return (
+    <div className="flex flex-col items-center gap-12 rounded-[16px] border border-gray-800 px-4 py-6 md:w-[500px] md:flex-row lg:px-6 lg:pb-12">
+      <input
+        type="text"
+        placeholder="Why microservice over monolith ?"
+        className="w-full place-self-start border-none bg-transparent text-gray-300 placeholder-gray-500 outline-none"
+      />
+      <button className="rounded-lg bg-purple-500 px-8 py-2 text-xl text-white transition-colors hover:bg-purple-600">
+        Send
+      </button>
+    </div>
+  );
+};
+
+export default FooterLeft;
