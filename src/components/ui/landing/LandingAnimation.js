ca2801bb7e336245a353bb8178fff2a62d0b82f5
jorjishasan <dev.jorjis@gmail.com>
2024-12-16 17:17:05 +0600

    refactor: simplify LandingAnimation component and enhance animation timing

--- src/components/ui/landing/LandingAnimation.js
+++ src/components/ui/landing/LandingAnimation.js
@@ -1,13 +1,13 @@
 import AnimatedList from "./AnimatedList";
 import Notification from "./Notification";
 import { notifications } from "@/components/ui/landing/notificationConfig";
-const LandingAnimation = ({ className = "" }) => {
+
+const LandingAnimation = () => {
   return (
     <div
-      className={`hidden h-[360px] w-auto overflow-hidden xl:visible xl:flex ${className}`}
-      style={{ transform: "rotate(-2deg)" }}
+      className={`hidden h-[360px] -rotate-2 transform-gpu overflow-hidden transition-transform duration-200 xl:visible xl:flex`}
     >
-      <AnimatedList delay={1000}>
+      <AnimatedList delay={1200}>
         {notifications.map((item, idx) => (
           <Notification {...item} index={idx} key={idx} />
         ))}
