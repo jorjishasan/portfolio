0a943b52e47551e8a97d8674be7c77ab14263d34
jorjishasan <dev.jorjis@gmail.com>
2024-12-16 19:37:12 +0600

    fix: add id attribute to Projects section for improved navigation targeting

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -12,6 +12,7 @@ const Projects = () => {
 
   return (
     <div
+      id="projects"
       className="mx-auto mt-section-margin-y p-4 py-section-padding-y md:mt-section-margin-y-md md:py-section-padding-y-md lg:mt-section-margin-y-lg lg:py-section-padding-y-lg"
       style={{ maxWidth: `${maxContainerWidth}px` }}
     >
