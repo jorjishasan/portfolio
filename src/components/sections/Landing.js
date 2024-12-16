e2b0de5ab6b81231fd0274b19cefabab66cef13a
jorjishasan <dev.jorjis@gmail.com>
2024-12-16 17:17:42 +0600

    style: reorganize class names in Landing component for improved readability

--- src/components/sections/Landing.js
+++ src/components/sections/Landing.js
@@ -8,8 +8,8 @@ const Landing = () => {
   return (
     <div
       className={cn(
-        "py-section-padding-y mt-section-margin-y md:py-section-padding-y-md lg:py-section-padding-y-lg md:mt-section-margin-y-md lg:mt-section-margin-y-lg bg-gradient-to-b from-blue-900 from-0% to-black to-100%",
-        "relative overflow-hidden pt-28 md:pt-32 lg:pt-36",
+        "mt-section-margin-y bg-gradient-to-b from-blue-900 from-0% to-black to-100% py-section-padding-y md:mt-section-margin-y-md md:py-section-padding-y-md lg:mt-section-margin-y-lg lg:py-section-padding-y-lg",
+        "relative overflow-hidden pt-28 md:pt-32 xl:pt-36",
       )}
       // style={{ marginTop: `${headerHeight}px` }}
     >
@@ -17,8 +17,8 @@ const Landing = () => {
         className="mx-auto xl:flex xl:justify-between"
         style={{ maxWidth: `${maxContainerWidth}px` }}
       >
-        <LandingContent className="" />
-        <LandingAnimation className="transform-gpu transition-all duration-300" />
+        <LandingContent />
+        <LandingAnimation />
       </div>
     </div>
   );
