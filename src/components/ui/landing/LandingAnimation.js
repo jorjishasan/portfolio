5585223fca3cc42440d7e854085cfb17fa1880da
jorjishasan <dev.jorjis@gmail.com>
2024-12-16 17:27:04 +0600

    fix: adjust rotation and delay in LandingAnimation for improved visual effect

--- src/components/ui/landing/LandingAnimation.js
+++ src/components/ui/landing/LandingAnimation.js
@@ -5,9 +5,9 @@ import { notifications } from "@/components/ui/landing/notificationConfig";
 const LandingAnimation = () => {
   return (
     <div
-      className={`hidden h-[360px] -rotate-2 transform-gpu overflow-hidden transition-transform duration-200 xl:visible xl:flex`}
+      className={`hidden h-[360px] -rotate-3 transform-gpu overflow-hidden transition-transform duration-200 xl:visible xl:flex`}
     >
-      <AnimatedList delay={1200}>
+      <AnimatedList delay={1500}>
         {notifications.map((item, idx) => (
           <Notification {...item} index={idx} key={idx} />
         ))}
