d6fced0b9b58fb41603a7d474c7bcbf4c90cf6b0
jorjishasan <dev.jorjis@gmail.com>
2024-12-17 01:37:22 +0600

    fix: remove margin-top from Landing section for improved layout consistency

--- src/components/sections/Landing.js
+++ src/components/sections/Landing.js
@@ -9,7 +9,7 @@ const Landing = () => {
     <div
       id="hola"
       className={cn(
-        "mt-section-margin-y bg-gradient-to-b from-blue-900 from-0% to-black to-100% py-section-padding-y md:mt-section-margin-y-md md:py-section-padding-y-md lg:mt-section-margin-y-lg lg:py-section-padding-y-lg",
+        "bg-gradient-to-b from-blue-900 from-0% to-black to-100% py-section-padding-y md:py-section-padding-y-md lg:py-section-padding-y-lg",
         "relative overflow-hidden pt-28 md:pt-32 xl:pt-36",
       )}
       // style={{ marginTop: `${headerHeight}px` }}
