eb08cb9dac290ed8a686d51bdb439e650e3e64b3
jorjishasan <dev.jorjis@gmail.com>
2024-12-16 17:16:54 +0600

    style: update LandingContent styles for improved responsiveness and button hover effect

--- src/components/ui/landing/LandingContent.js
+++ src/components/ui/landing/LandingContent.js
@@ -6,14 +6,14 @@ const LandingContent = () => {
       <span className="text-base md:text-xl lg:text-left lg:text-2xl">
         ☘️ I&apos;m Jorjis Hasan,
       </span>
-      <h1 className="mb-8 mt-4 text-5xl font-black leading-tight md:text-6xl md:leading-[1.4] lg:mb-6">
+      <h1 className="mb-8 mt-4 text-5xl font-black leading-tight md:text-6xl md:leading-[1.4] lg:mb-6 xl:text-[56px]">
         10x <br />
         Full-Stack Software <br /> Developer
         <span className="text-purple-500"> .</span>
       </h1>
 
       <div className="md:text-md flex flex-col items-center justify-center gap-2 text-xs md:mt-10 lg:mt-16 lg:flex-row lg:gap-4 lg:text-lg xl:justify-normal">
-        <button className="w-fit rounded-md bg-purple-500 px-8 py-3 font-medium text-white lg:py-4">
+        <button className="w-fit rounded-md bg-purple-600 px-8 py-3 font-medium text-white hover:bg-purple-700 lg:py-4 xl:py-[14px]">
           Preview Resume
         </button>
 
