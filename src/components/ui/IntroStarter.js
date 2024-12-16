a7f3009759faca04ab7592ecd2bfe43a01a41258
jorjishasan <dev.jorjis@gmail.com>
2024-12-17 02:51:18 +0600

    style: enhance padding and text alignment in IntroStarter component for better layout

--- src/components/ui/IntroStarter.js
+++ src/components/ui/IntroStarter.js
@@ -2,12 +2,12 @@
 
 const IntroStarter = ({ tag, title, description }) => {
   return (
-    <div className="mx-auto mb-16 w-full text-center">
+    <div className="mx-auto mb-16 w-full p-4 text-center">
       <span className="mb-2 inline-block bg-purple-500/10 px-4 py-2 text-[10px] font-bold uppercase text-purple-500">
         {tag}
       </span>
       <h2 className="text-2xl font-bold md:text-4xl lg:text-4xl">{title}</h2>
-      <p className="mx-auto mt-4 max-w-xs text-center text-sm text-gray-500/80 md:max-w-sm md:text-left md:text-sm lg:max-w-md lg:text-base">
+      <p className="mx-auto mt-4 max-w-xs text-center text-sm text-gray-500 md:max-w-sm md:text-left md:text-sm lg:max-w-md lg:text-base">
         {description}
       </p>
     </div>
