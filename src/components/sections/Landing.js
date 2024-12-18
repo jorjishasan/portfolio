c1574f720cc60e9847ac5e1adba6cb8c911c7af9
jorjishasan <dev.jorjis@gmail.com>
2024-12-19 03:26:17 +0600

    feat: conditionally render LandingAnimation based on desktop view

--- src/components/sections/Landing.js
+++ src/components/sections/Landing.js
@@ -4,7 +4,11 @@ import LandingContent from "@/components/ui/landing/LandingContent";
 import LandingAnimation from "@/components/ui/landing/LandingAnimation";
 import cn from "@/utils/cn";
 import { maxContainerWidth } from "@/utils/constants";
+import { useIsDesktop } from "@/hooks/useIsDesktop";
+
 const Landing = () => {
+  const isDesktop = useIsDesktop();
+
   return (
     <div
       id="hola"
@@ -12,14 +16,13 @@ const Landing = () => {
         "bg-gradient-to-b from-blue-900 from-0% to-black to-100% py-section-padding-y md:py-section-padding-y-md lg:py-section-padding-y-lg",
         "relative overflow-hidden pt-28 md:pt-32 xl:pt-36",
       )}
-      // style={{ marginTop: `${headerHeight}px` }}
     >
       <div
         className="mx-auto xl:flex xl:justify-between"
         style={{ maxWidth: `${maxContainerWidth}px` }}
       >
         <LandingContent />
-        <LandingAnimation />
+        {isDesktop && <LandingAnimation />}
       </div>
     </div>
   );
