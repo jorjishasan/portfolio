329ffcb84c2da5ff8a692367b498b1976e8c07b7
jorjishasan <dev.jorjis@gmail.com>
2024-12-19 04:03:15 +0600

    refactor: adjust delay in AnimatedList for improved landing animation timing

--- src/components/ui/landing/LandingAnimation.js
+++ src/components/ui/landing/LandingAnimation.js
@@ -8,7 +8,7 @@ const LandingAnimation = () => {
     <div
       className={`flex h-[360px] -rotate-3 transform-gpu overflow-hidden transition-transform duration-200`}
     >
-      <AnimatedList delay={1500}>
+      <AnimatedList delay={1200}>
         {notifications.map((item, idx) => (
           <Notification {...item} index={idx} key={idx} />
         ))}
