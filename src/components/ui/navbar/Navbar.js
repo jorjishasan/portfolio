4d611eaa9ac2d0dd20e0308bd0acb35473e83016
jorjishasan <dev.jorjis@gmail.com>
2024-12-20 10:45:55 +0600

    refactor: adjust padding in Navbar component for improved layout

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -5,7 +5,7 @@ import QuickLinksMenu from "./QuickLinksMenu";
 const Navbar = () => {
   return (
     <nav
-      className="relative mx-auto flex items-center justify-between px-2 py-4 transition-all duration-300 lg:px-0"
+      className="relative mx-auto flex items-center justify-between p-4 transition-all duration-300 lg:px-0"
       style={{ maxWidth: `${maxContainerWidth}px` }}
     >
       <LeftNav />
