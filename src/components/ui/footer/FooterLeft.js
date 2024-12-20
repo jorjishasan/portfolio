3d4037747f7ca07eca17d7df0891011c9408b1be
jorjishasan <dev.jorjis@gmail.com>
2024-12-20 13:21:08 +0600

    refactor: enhance layout and styling in FooterLeft component for better responsiveness

--- src/components/ui/footer/FooterLeft.js
+++ src/components/ui/footer/FooterLeft.js
@@ -17,13 +17,13 @@ const FooterLeft = () => {
 
 const InputBox = () => {
   return (
-    <div className="flex flex-col items-center gap-12 rounded-[16px] border border-gray-800 px-4 py-6 md:w-[500px] md:flex-row md:pb-12 lg:px-6 lg:pb-14">
+    <div className="flex flex-col items-center gap-12 rounded-[16px] border border-gray-800 px-4 py-6 md:w-[500px] lg:w-[550px] lg:px-6 lg:py-8 xl:w-[650px] xl:gap-16">
       <input
         type="text"
         placeholder="Why microservice over monolith ?"
-        className="w-full place-self-start border-none bg-transparent text-gray-300 placeholder-gray-500 outline-none"
+        className="w-full place-self-start text-wrap bg-transparent text-xl text-gray-300 placeholder-gray-500 outline-none"
       />
-      <button className="rounded-lg bg-purple-500 px-8 py-2 text-xl text-white transition-colors hover:bg-purple-600">
+      <button className="rounded-lg bg-purple-600 px-8 py-3 text-lg text-white transition-colors hover:bg-purple-600 md:text-[24px] xl:px-16 xl:py-4">
         Send
       </button>
     </div>
